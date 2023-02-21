094d10b978b72b4bfcad4e4f15f95b4a7a6307da
PanzerWeb <selwynvillar@gmail.com>
2023-02-21 11:56:50 +0800

    Too much

--- app.js
+++ app.js
@@ -14,12 +14,48 @@ function save(){
     let entryCount = count + " - ";
 
     if(count === previousCount){
-        alert("MULTIPLE ENTRIES DETECTED!!!");
+        return entryCount;
     }
+
     else{
         previousCount = count;
         saveEl.innerHTML = saveEl.innerHTML + " " + entryCount;
     }
 }
 
-console.log("hello world");
+//CODE TO CLEAR AND SET THE COUNT INTO ZERO, RESETTING THE WHOLE FUNCTION EXCEPT FOR THE SAVE ENTRIES
+function clear(){
+    count = 0;
+    countEl.innerHTML = count;
+
+}
+
+var clearButton = document.getElementById('clear-btn');
+clearButton.addEventListener('click', clear);
+
+
+//-------------------------------------------------------
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+const alert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('save-btn')
+if (alertTrigger) {
+  const handleClick = () => {
+    alert('Repeated Numbers is Invalid', 'success')
+    alertTrigger.removeEventListener('click', handleClick)
+  }
+
+  alertTrigger.addEventListener('click', handleClick)
+}
+
